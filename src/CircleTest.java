e2c3bb2a6a15dc06d287f59183c25f5308d72aeb
binhsonnguyen <son@binhsonnguyen.com>
2018-02-21 15:57:48 +0700

    circle class and its testing program

---
+++ src/CircleTest.java
@@ -0,0 +1,12 @@
+public class CircleTest {
+    public static void main(String[] args) {
+        Circle circle = new Circle();
+        System.out.println(circle);
+
+        circle = new Circle(3.5);
+        System.out.println(circle);
+
+        circle = new Circle("indigo", false, 3.5);
+        System.out.println(circle);
+    }
+}
