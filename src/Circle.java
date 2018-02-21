33994fb82070242b6ab1a576fbf257c269bbae91
binhsonnguyen <son@binhsonnguyen.com>
2018-02-21 16:12:01 +0700

    rearange constructors' args

--- src/Circle.java
+++ src/Circle.java
@@ -8,7 +8,7 @@ public class Circle extends Shape {
         this.radius = radius;
     }
 
-    public Circle(String color, boolean filled, double radius) {
+    public Circle(double radius, String color, boolean filled) {
         super(color, filled);
         this.radius = radius;
     }
--- src/CircleTest.java
+++ src/CircleTest.java
@@ -6,7 +6,7 @@ public class CircleTest {
         circle = new Circle(3.5);
         System.out.println(circle);
 
-        circle = new Circle("indigo", false, 3.5);
+        circle = new Circle(3.5, "indigo", false);
         System.out.println(circle);
     }
 }
